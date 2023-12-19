3d9020e9195c65b6808efcd00c76a35819827b3f
Weronika Borucka <wborucka@kbj.pl>
2023-12-19 14:50:22 +0100

    added yamls and working app

--- translate/translate.py
+++ translate/translate.py
@@ -2,14 +2,16 @@ from flask import Flask
 import requests
 import os
 import logging
+import urllib.parse
+import json
 
 log = logging.getLogger("translate")
-logging.basicConfig(level=20, format='%(asctime)s %(levelname)s: %(message)s')
+logging.basicConfig(level=10, format='%(asctime)s %(levelname)s: %(message)s')
 
 translate_fun = os.getenv("FUN", 'False').lower() in ('true', '1', 't', 'True')
 shrek_service_url = os.getenv("SHREK_SERVICE_URL","http://0.0.0.0:5000")
 
-translate_api_url_fun = "https://api.funtranslations.com/translate/doge.json"
+translate_api_url_fun = "https://api.funtranslations.com/translate/minion.json"
 translate_api_url_ru = "https://libretranslate.com/translate"
 
 print(translate_fun)
@@ -19,7 +21,7 @@ app = Flask(__name__)
 @app.route("/translate-shrek")
 def translate_shrek():
     shrek_quote_response = requests.get(f"{shrek_service_url}/shrek")
-    shrek_quote = shrek_quote_response.content
+    shrek_quote = shrek_quote_response.text
     log.info(f"shrek Quote from server: {shrek_quote}")
     print(shrek_quote)
     if translate_fun == True:
@@ -31,7 +33,7 @@ def translate_shrek():
 @app.route("/translate-donkey")
 def translate_donkey():
     donkey_quote_response = requests.get(f"{shrek_service_url}/donkey")
-    donkey_quote = donkey_quote_response.content
+    donkey_quote = donkey_quote_response.text
     log.info(f"Donkey Quote from server: {donkey_quote}")
     if translate_fun == True:
         response = translate_to_fun(donkey_quote)
@@ -40,20 +42,20 @@ def translate_donkey():
     return response
 
 def translate_to_fun(quote_text: str):
-    to_translate = f"text={quote_text}"
-    translated_response = requests.post(translate_api_url_fun, data=to_translate, headers={"X-Funtranslations-Api-Secret": "<api_key>"})
+    to_translate = f"text={urllib.parse.quote(quote_text, safe='')}"
+    translated_response = requests.post(translate_api_url_fun, params=to_translate, headers={"Content-Type": "application/x-www-form-urlencoded", "X-Funtranslations-Api-Secret": "<api_key>"})
     translated = translated_response.content
     log.info(f"translated: {translated}")
     return translated
 
 def translate_to_ru(quote_text: str):
-    to_translate = {
-        "q": quote_text,
+    to_translate = json.dumps({
+        "q": urllib.parse.quote(quote_text, safe=''),
         "source": "auto",
-        "target": "ru",
+        "target": "pl",
         "format": "text",
         "api_key": ""
-	}
+	})
     translated_response = requests.post(translate_api_url_ru, data=to_translate, headers={"Content-Type": "application/json"})
     translated = translated_response.content
     log.info(f"translated: {translated}")
